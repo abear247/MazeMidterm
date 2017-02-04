87c42ae04ab2cddbb7d4104f623dee7930b7cea6
mlinglhl <minhung.ling@gmail.com>
2017-02-03 20:02:58 -0500

    Initial Commit

---
+++ FlickrMaze/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  FlickrMaze
+//
+//  Created by Minhung Ling on 2017-02-03.
+//  Copyright © 2017 Minhung Ling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ FlickrMaze/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FlickrMaze
+//
+//  Created by Minhung Ling on 2017-02-03.
+//  Copyright © 2017 Minhung Ling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
