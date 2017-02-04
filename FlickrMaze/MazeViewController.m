162f93ac90429e2d00d5c133d669533175193274
mlinglhl <minhung.ling@gmail.com>
2017-02-04 13:15:28 -0500

    Added methods for moving

--- FlickrMaze/MazeViewController.m
+++ FlickrMaze/MazeViewController.m
@@ -14,7 +14,9 @@
 
 @interface MazeViewController ()
 
-@property UICollectionView *collectionView;
+@property UICollectionView *mazeCollectionView;
+@property int rowCount;
+@property int sectionCount;
 
 @end
 
@@ -23,7 +25,12 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self.manager generateMaze];
-    self.collectionView.collectionViewLayout = [MazeFlowLayout new];
+    self.rowCount = 3;
+    self.sectionCount = 3;
+    UICollectionViewFlowLayout *layout = (UICollectionViewFlowLayout *)self.mazeCollectionView.collectionViewLayout;
+    CGFloat width = self.mazeCollectionView.frame.size.width/3;
+    CGSize size = CGSizeMake(width, width);
+    layout.itemSize = size;
 }
 
 - (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
@@ -36,7 +43,7 @@
 }
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
-    MazeCell *cell = [self.collectionView dequeueReusableCellWithReuseIdentifier:@"MazeCell" forIndexPath:indexPath];
+    MazeCell *cell = [self.mazeCollectionView dequeueReusableCellWithReuseIdentifier:@"MazeCell" forIndexPath:indexPath];
     NSArray *array = [self.manager getArray];
     NSArray *tileArray = array[indexPath.section];
     MazeTile *tile = tileArray[indexPath.row];
@@ -45,6 +52,45 @@
         cell.mazeCellImageView.image = [UIImage imageWithData:data];
     }
     return cell;
+}
+
+- (void) moveLeft:(UIButton *)sender {
+    self.rowCount -= 1;
+    [self.mazeCollectionView deleteItemsAtIndexPaths:@[[NSIndexPath indexPathForRow:2 inSection:0],
+                                                       [NSIndexPath indexPathForRow:2 inSection:1],
+                                                       [NSIndexPath indexPathForRow:2 inSection:2]
+                                                       ]];
+    self.rowCount += 1;
+    [self.mazeCollectionView insertItemsAtIndexPaths:@[
+                                                       [NSIndexPath indexPathForRow:0 inSection:0],
+                                                       [NSIndexPath indexPathForRow:0 inSection:1],
+                                                       [NSIndexPath indexPathForRow:0 inSection:2]                                                       ]];
+}
+
+- (void)moveRight:(UIButton *)sender {
+    self.rowCount -= 1;
+    [self.mazeCollectionView deleteItemsAtIndexPaths:@[[NSIndexPath indexPathForRow:0 inSection:0],
+                                                       [NSIndexPath indexPathForRow:0 inSection:1],
+                                                       [NSIndexPath indexPathForRow:0 inSection:2]
+                                                       ]];
+    self.rowCount += 1;
+    [self.mazeCollectionView insertItemsAtIndexPaths:@[
+                                                       [NSIndexPath indexPathForRow:2 inSection:0],
+                                                       [NSIndexPath indexPathForRow:2 inSection:1],
+                                                       [NSIndexPath indexPathForRow:2 inSection:2]                                                       ]];
+}
+- (void)moveUp:(UIButton *)sender {
+    self.sectionCount += 1;
+    [self.mazeCollectionView insertSections:[NSIndexSet indexSetWithIndex:0]];
+    self.sectionCount -= 1;
+    [self.mazeCollectionView deleteSections:[NSIndexSet indexSetWithIndex:3]];
+}
+
+- (void)moveDown:(UIButton *)sender {
+    self.sectionCount += 1;
+    [self.mazeCollectionView insertSections:[NSIndexSet indexSetWithIndex:3]];
+    self.sectionCount -= 1;
+    [self.mazeCollectionView deleteSections:[NSIndexSet indexSetWithIndex:0]];
 }
 
 @end
