dfcdad87e62c3a66ad9377f129628a46ca199cd1
mlinglhl <minhung.ling@gmail.com>
2017-02-05 15:22:41 -0500

    Added ghost position to player object

--- MazeTile+CoreDataProperties.m
+++ MazeTile+CoreDataProperties.m
@@ -2,7 +2,7 @@
 //  MazeTile+CoreDataProperties.m
 //  FlickrMaze
 //
-//  Created by Minhung Ling on 2017-02-04.
+//  Created by Minhung Ling on 2017-02-05.
 //  Copyright © 2017 Minhung Ling. All rights reserved.
 //  This file was automatically generated and should not be edited.
 //
