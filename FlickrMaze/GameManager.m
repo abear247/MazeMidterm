c48dec9409c7a8503bc0909a6d3a3862f89edb27
mlinglhl <minhung.ling@gmail.com>
2017-02-09 13:48:55 -0500

    Score calculation changes

--- FlickrMaze/GameManager.m
+++ FlickrMaze/GameManager.m
@@ -421,17 +421,16 @@
 - (void) calculateScore {
     NSManagedObjectContext *context = [self getContext];
     NSInteger playerScore = 20* self.maze.minMoves / (self.player.moveCount+1) + self.player.time*50;
-    self.playerScore = playerScore;
-    
     if (!self.practiceMode) {
         ScoreKeeper *score = [[ScoreKeeper alloc] initWithContext:context];
         score.playerName = self.player.name;
         score.playerImage = self.player.image;
-        score.score = playerScore;
         if (self.player.gameWon) {
-            score.score *= 300;
+            playerScore *= 3;
             score.playerWon = self.player.gameWon;
         }
+        self.playerScore = playerScore;
+        score.score = playerScore;
         score.moves = self.player.moveCount;
         score.map = self.player.mazeID;
         score.playerTime = self.player.time;
